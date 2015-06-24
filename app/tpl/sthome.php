9775c7ffc736d03582a24a32ca24eaa04214b54f
AidaDC <AidaDC@users.noreply.github.com>
2015-06-24 23:02:02 +0200

    Update sthome.php

--- app/tpl/sthome.php
+++ app/tpl/sthome.php
@@ -1,27 +1,27 @@
-﻿<div id="titulotira">Novedades!</div>
+<div id="titulotira">Novedades!</div>
 <div id="libros">
 <div id="libro">
 <div id="izq"><img id="imagen" src="http://2daw13.cesnuria.com/img/harrypotterylasreliquiasdelamuerte.jpg" alt="Portada Harry Potter 7"></div>
 <div id="der"><div id="descrip"><p>Nombre: Harry Potter y las reliquias de la muerte</p><p>Autor:J.K. Rowling</p><p>Precio:  25.5</p><p>Stock:  0</p></div>
 <div id="der2">
-<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="/to-2dawcredito.rhcloud.com/gestionar/gestionarlibros"><input id="comp" type="submit" value="Más  Información" name="enviar">
-</form><a id="carrito" href="/to-2dawcredito.rhcloud.com/carrito/carrito">Añadir al Carrito</a></div>
+<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="'<?= . APP_W . 'gestionar/gestionarlibros;'?>"><input id="comp" type="submit" value="Más  Información" name="enviar">
+<a id="carrito" href="'<?= . APP_W . 'carrito/carrito;'?>">Añadir al Carrito</a></form></div>
 </div> <!-- final del div der -->
 <input type="text" style="display:none;" name="id" value="1"></div><!-- final del div libro -->
 <div id="libro">
 <div id="izq"><img id="imagen" src="http://2daw13.cesnuria.com/img/brisingr.png" alt="Portada Brisingr"></div>
 <div id="der"><div id="descrip"><p>Nombre: Brisingr</p><p>Autor: Cristopher Paolini</p><p>Precio:40</p><p>Stock:3</p></div>
 <div id="der2">
-<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="/to-2dawcredito.rhcloud.com/gestionar/gestionarlibros"><input id="comp" type="submit" value="Más  Información" name="enviar">
-</form><a id="carrito" href="/to-2dawcredito.rhcloud.com/carrito/carrito">Añadir al Carrito</a></div>
+<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="'<?= . APP_W . 'gestionar/gestionarlibros;'?>"><input id="comp" type="submit" value="Más  Información" name="enviar">
+<a id="carrito" href="'<?= . APP_W . 'carrito/carrito;'?>">Añadir al Carrito</a></form></div>
 </div> <!-- final del div der -->
 <input type="text" style="display:none;" name="id" value="1"></div><!-- final del div libro -->
 <div id="libro">
 <div id="izq"><img id="imagen" src="http://2daw13.cesnuria.com/img/eljuegodeender.jpg" alt="Portada El Juego de Ender"></div>
 <div id="der"><div id="descrip"><p>Nombre:  El juego de Ender</p><p>Autor:Orson Scott Card</p><p>Precio:  25.5</p><p>Stock:  3</p></div>
 <div id="der2">
-<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="/to-2dawcredito.rhcloud.com/gestionar/gestionarlibros"><input id="comp" type="submit" value="Más  Información" name="enviar">
-</form><a id="carrito" href="/to-2dawcredito.rhcloud.com/carrito/carrito">Añadir al Carrito</a></div>
+<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="'<?= . APP_W . 'gestionar/gestionarlibros;'?>"><input id="comp" type="submit" value="Más  Información" name="enviar">
+</form><a id="carrito" href="'<?= . APP_W . 'carrito/carrito;'?>">Añadir al Carrito</a></div>
 </div> <!-- final del div der -->
 <input type="text" style="display:none;" name="id" value="1"></div><!-- final del div libro -->
 </div> <!-- final del div libros -->
@@ -31,24 +31,24 @@
 <div id="izq"><img id="imagen" src="http://2daw13.cesnuria.com/img/harrypotterylaordendelfenix.png" alt="Portada Harry Potter 5"></div>
 <div id="der"><div id="descrip"><p>Nombre:  Harry Potter y la orden del fenix</p><p>Autor:J.K. Rowling</p><p>Precio:  25.5</p><p>Stock:  3</p></div>
 <div id="der2">
-<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="/to-2dawcredito.rhcloud.com/gestionar/gestionarlibros"><input id="comp" type="submit" value="Más  Información" name="enviar">
-</form><a id="carrito" href="/to-2dawcredito.rhcloud.com/carrito/carrito">Añadir al Carrito</a></div>
+<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="'<?= . APP_W . 'gestionar/gestionarlibros;'?>"><input id="comp" type="submit" value="Más  Información" name="enviar">
+<a id="carrito" href="'<?= . APP_W . 'carrito/carrito;'?>">Añadir al Carrito</a></form></div>
 </div> <!-- final del div der -->
 <input type="text" style="display:none;" name="id" value="1"></div><!-- final del div libro -->
 <div id="libro">
 <div id="izq"><img id="imagen" src="http://2daw13.cesnuria.com/img/Legado.jpg" alt="Portada Legado"></div>
 <div id="der"><div id="descrip"><p>Nombre:  Legado</p><p>Autor:Cristopher Paolini</p><p>Precio:  40</p><p>Stock:  0</p></div>
 <div id="der2">
-<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="/to-2dawcredito.rhcloud.com/gestionar/gestionarlibros"><input id="comp" type="submit" value="Más  Información" name="enviar">
-</form><a id="carrito" href="/to-2dawcredito.rhcloud.com/carrito/carrito">Añadir al Carrito</a></div>
+<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="'<?= . APP_W . 'gestionar/gestionarlibros;'?>"><input id="comp" type="submit" value="Más  Información" name="enviar">
+<a id="carrito" href="'<?= . APP_W . 'carrito/carrito;'?>">Añadir al Carrito</a></form></div>
 </div> <!-- final del div der -->
 <input type="text" style="display:none;" name="id" value="1"></div><!-- final del div libro -->
 <div id="libro">
 <div id="izq"><img id="imagen" src="http://2daw13.cesnuria.com/img/elgremiodelosmagos.jpg" alt="Portada El Gremio de los magos"></div>
 <div id="der"><div id="descrip"><p>Nombre: El gremio de los magos</p><p>Autor:Trudi Canavan</p><p>Precio:  25.5</p><p>Stock:  3</p></div>
 <div id="der2">
-<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="/http://to-2dawcredito.rhcloud.com//gestionar/gestionarlibros"><input id="comp" type="submit" value="Más  Información" name="enviar">
-</form><a id="carrito" href="/to-2dawcredito.rhcloud.com/carrito/carrito">Añadir al Carrito</a></div>
+<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="'<?= . APP_W . 'gestionar/gestionarlibros;'?>"><input id="comp" type="submit" value="Más  Información" name="enviar">
+<a id="carrito" href="'<?= . APP_W . 'carrito/carrito;'?>">Añadir al Carrito</a></form></div>
 </div> <!-- final del div der -->
 <input type="text" style="display:none;" name="id" value="1"></div><!-- final del div libro -->
 </div> <!-- final del div libros -->
@@ -58,24 +58,24 @@
 <div id="izq"><img id="imagen" src="http://2daw13.cesnuria.com/img/harrypotterylacamarasecreta.jpg" alt="Portada Harry Potter 2"></div>
 <div id="der"><div id="descrip"><p>Nombre: Harry Potter y la cámara secreta</p><p>Autor:J.K.Rowling</p><p>Precio:  25.5</p><p>Stock:  3</p></div>
 <div id="der2">
-<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="/to-2dawcredito.rhcloud.com/gestionar/gestionarlibros"><input id="comp" type="submit" value="Más  Información" name="enviar">
-</form><a id="carrito" href="/to-2dawcredito.rhcloud.com/carrito/carrito">Añadir al Carrito</a></div>
+<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="'<?= . APP_W . 'gestionar/gestionarlibros;'?>"><input id="comp" type="submit" value="Más  Información" name="enviar">
+<a id="carrito" href="'<?= . APP_W . 'carrito/carrito;'?>">Añadir al Carrito</a></form></div>
 </div> <!-- final del div der -->
 <input type="text" style="display:none;" name="id" value="1"></div><!-- final del div libro -->
 <div id="libro">
 <div id="izq"><img id="imagen" src="http://2daw13.cesnuria.com/img/eldest.jpg" alt="Portada Eldest"></div>
 <div id="der"><div id="descrip"><p>Nombre:  Eldest</p><p>Autor:Cristopher Paolini</p><p>Precio:  25.5</p><p>Stock:  3</p></div>
 <div id="der2">
-<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="/to-2dawcredito.rhcloud.com/gestionar/gestionarlibros"><input id="comp" type="submit" value="Más  Información" name="enviar">
-</form><a id="carrito" href="/to-2dawcredito.rhcloud.com/carrito/carrito">Añadir al Carrito</a></div>
+<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="'<?= . APP_W . 'gestionar/gestionarlibros;'?>"><input id="comp" type="submit" value="Más  Información" name="enviar">
+<a id="carrito" href="'<?= . APP_W . 'carrito/carrito;'?>">Añadir al Carrito</a></form></div>
 </div> <!-- final del div der -->
 <input type="text" style="display:none;" name="id" value="1"></div><!-- final del div libro -->
 <div id="libro">
 <div id="izq"><img id="imagen" src="http://2daw13.cesnuria.com/img/JuegodeTronos.jpg" alt="Portada Juego de tronos"></div>
 <div id="der"><div id="descrip"><p>Nombre:   Juego de Tronos</p><p>Autor: George R.R. Martin</p><p>Precio:  25.5</p><p>Stock:  3</p></div>
 <div id="der2">
-<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="/to-2dawcredito.rhcloud.com/gestionar/gestionarlibros"><input id="comp" type="submit" value="Más  Información" name="enviar">
-</form><a id="carrito" href="/to-2dawcredito.rhcloud.com/carrito/carrito">Añadir al Carrito</a></div>
+<form id="comp" name="form1" method="post" accept-charset="UTF-8" action="'<?= . APP_W . 'gestionar/gestionarlibros;'?>"><input id="comp" type="submit" value="Más  Información" name="enviar">
+<a id="carrito" href="'<?= . APP_W . 'carrito/carrito;'?>">Añadir al Carrito</a></form></div>
 </div> <!-- final del div der -->
 <input type="text" style="display:none;" name="id" value="1"></div><!-- final del div libro -->
 </div> <!-- final del div libros -->
